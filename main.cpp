9f1019f44d4befa10d53e32a360be304d8771b1d
Robiks99 <98769851+Robiks99@users.noreply.github.com>
2022-02-07 22:56:06 +0100

    Added frame rate limit

--- main.cpp
+++ main.cpp
@@ -2,15 +2,27 @@
 #include"Game.h"
 
 int main(int argc, const char* argv[]){
-Game *game = new Game();
+    Game *game = new Game();
 
-game->init("Pong",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, 800, 600, false);
+    const int FPS = 60;
+    const int frameDelay = 1000/FPS;
+
+    Uint32 frameStart;
+    int frameTime;
+
+
+    game->init("Pong",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, 800, 600, false);
+    
     while(game->running()){
+        frameStart = SDL_GetTicks();
 
         game->handleEvents();
         game->update();
         game->render();
 
+        frameTime = SDL_GetTicks() - frameStart;
+        if(frameDelay > frameTime) SDL_Delay(frameDelay - frameTime);
+        
     }
 
     game->clean();
