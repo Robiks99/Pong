894757403f78a72ccffa618beebbf346762262a9
Robiks99 <98769851+Robiks99@users.noreply.github.com>
2022-02-24 18:54:39 +0100

    Changed Enemy AI

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,32 +1,36 @@
 #include"Enemy.h"
 Enemy::Enemy(SDL_Renderer* Renderer, SDL_Window* Window) : Entity(Renderer, Window){
-
+    speed = 80;
 }
 Enemy::Enemy(SDL_Renderer* Renderer, SDL_Window* Window, int XPos, int YPos, int Height, int Width) :
- Entity(Renderer, Window, XPos, YPos, Height, Width){
-     
+Entity(Renderer, Window, XPos, YPos, Height, Width){
+    speed = 80;
 
 }
 
 void Enemy::move(Ball* Ball){
-    if(Ball->xVel < 0){
+    if(Ball->xVel < 0){// if ball is going toward player
         if((yPos-(height/2)) > ((windowHeight/2)+32)){
-            yVel = -speed;
+            yVel = -1;
             return;
         }
         if((yPos-(height/2)) < ((windowHeight/2)-32)){
-            yVel = speed;
+            yVel = 1;
             return;
         }
         yVel = 0;
         return;
     }
-    if(Ball->xVel > 0){
-        if(Ball->yPos < yPos){
-            yVel = -speed;
+    if(Ball->xVel > 0){//if ball is going toward enemy
+        if((Ball->yPos+(Ball->height/2) + 16) < yPos + height/2){
+            yVel = -1;
+            return;
         }
-        if(Ball->yPos > yPos){
-            yVel = speed;
+        if((Ball->yPos+(Ball->height/2) - 16) > yPos + height/2){
+            yVel = 1;
+            return;
         }
+        yVel = 0;
+        return;
     }
 }
