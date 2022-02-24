9e409eae5498704ae0fb68f867f332c1c726c768
Robiks99 <98769851+Robiks99@users.noreply.github.com>
2022-02-24 19:40:33 +0100

    Added menu loop

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 
 int main(int argc, const char* argv[]){
     Game *game = new Game();
-
     const int FPS = 60;
     const int frameDelay = 1000/FPS;
 
@@ -14,12 +13,23 @@ int main(int argc, const char* argv[]){
     game->init("Pong",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, 800, 600, false);
     
     while(game->running()){
+         while(game->menu()){
+            frameStart = SDL_GetTicks();
+            
+            game->menuHandleEvents();
+            game->menuUpdate();
+            game->menuRender();
+
+            frameTime = SDL_GetTicks() - frameStart;
+            if(frameDelay > frameTime) SDL_Delay(frameDelay - frameTime);
+        }
         frameStart = SDL_GetTicks();
+
 
         game->handleEvents();
         game->update();
         game->render();
-
+        
         frameTime = SDL_GetTicks() - frameStart;
         if(frameDelay > frameTime) SDL_Delay(frameDelay - frameTime);
         
